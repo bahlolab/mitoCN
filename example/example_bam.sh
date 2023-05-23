cfe6d1dca28acda2e790eb46a9fce25a133fa74d
Longfei Wang <54878372+longfei-stat@users.noreply.github.com>
2023-05-23 15:11:41 +1000

    Delete example directory
    remove example directory

--- example/example_bam.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# BAM example with hg19 ref 
-# estimate mtDNA-CN using chromosome 20
-mitoCN_PATH=/stornext/Bioinf/data/lab_bahlo/users/wang.lo/git/mitoCN
-BAM=${mitoCN_PATH}/data/sample2.bam
-out=${mitoCN_PATH}/example
-
-bash ${mitoCN_PATH}/mitoCN.sh -d ${mitoCN_PATH} -f $BAM -v hg19 -m chrM -k chr20 -o $out
