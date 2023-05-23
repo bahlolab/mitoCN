7186a66d2862290cf7235a79c749ba4017c2d16c
Longfei Wang <54878372+longfei-stat@users.noreply.github.com>
2023-05-23 15:21:32 +1000

    Update reads_sim.sh
    update reads sim

--- example/reads_sim.sh
+++ example/reads_sim.sh
@@ -1,5 +1,6 @@
 # ART-MountRainier-2016-06-05
 # art_illumina -ss HSXn -sam -i <seq_ref_file> -l 150 -f <fold_coverage> -o <outfile_prefix>
+art_illumina=/home/users/allstaff/wang.lo/lab_bahlo_home/software/art_bin_MountRainier/art_illumina
 
 # extract chr20, chrX, chrY, chrM from reference fa file
 samtools faidx Homo_sapiens_assembly38.fasta chrM > ./reads_sim/human38_chrM.fa
@@ -10,10 +11,10 @@ samtools faidx Homo_sapiens_assembly38.fasta chr20 chrX chrY chrM > ./reads_sim/
 
 # simulate reads: male, mtDNA-CN = 3, average coverage = 6X
 cd reads_sim
-/home/users/allstaff/wang.lo/lab_bahlo_home/software/art_bin_MountRainier/art_illumina -ss HSXn -sam -i human38_chrM.fa -p -l 150 -f 9 -m 200 -s 10 -o chrM
-/home/users/allstaff/wang.lo/lab_bahlo_home/software/art_bin_MountRainier/art_illumina -ss HSXn -sam -i human38_chrX.fa -p -l 150 -f 3 -m 200 -s 10 -o chrX
-/home/users/allstaff/wang.lo/lab_bahlo_home/software/art_bin_MountRainier/art_illumina -ss HSXn -sam -i human38_chrY.fa -p -l 150 -f 3 -m 200 -s 10 -o chrY
-/home/users/allstaff/wang.lo/lab_bahlo_home/software/art_bin_MountRainier/art_illumina -ss HSXn -sam -i human38_chr20.fa -p -l 150 -f 6 -m 200 -s 10 -o chr20
+$art_illumina -ss HSXn -sam -i human38_chrM.fa -p -l 150 -f 9 -m 200 -s 10 -o chrM
+$art_illumina -ss HSXn -sam -i human38_chrX.fa -p -l 150 -f 3 -m 200 -s 10 -o chrX
+$art_illumina -ss HSXn -sam -i human38_chrY.fa -p -l 150 -f 3 -m 200 -s 10 -o chrY
+$art_illumina -ss HSXn -sam -i human38_chr20.fa -p -l 150 -f 6 -m 200 -s 10 -o chr20
 
 # sort and index sam -> bam
 samtools sort chrM.sam > chrM.bam
@@ -33,8 +34,3 @@ samtools index sample.bam
 # convert bam to cram
 samtools view -T human38_select.fa -C -o sample.cram sample.bam
 samtools index sample.cram
-
-
-
-
-
